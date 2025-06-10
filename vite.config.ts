eaed9c07c3a74918aeb9641957bdd9c6d6307d62
dhlee <dhlee.xionits.com>
2025-06-10 09:04:56 +0900

    fix: preview상태에서 설치배너 안뜨던 이슈 해결

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,27 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      includeAssets: ["favicon.svg", "pwa-192x192.png", "pwa-512x512.png"],
       manifest: {
-        name: "My PWA App",
-        short_name: "PWA App",
-        description: "A PWA example using Vite + React",
-        theme_color: "#ffffff",
+        name: "Operation Friday",
+        short_name: "Friday",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#010203",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
       },
     }),
   ],
