ae63ab80543016b4cd570945f2b5984767ed05d7
Haru-arp <vr1997@naver.com>
2025-06-13 00:02:01 +0900

    error: 페이지 타이틀 가져오는 custom hook 수정

--- src/hook/usePageTitle.tsx
+++ src/hook/usePageTitle.tsx
@@ -1,14 +1,14 @@
-import { router } from "@/constants/routeItems";
+import { menuItems } from "@/constants/routeItems";
 import { useMemo } from "react";
 import { useLocation } from "react-router-dom";
 
 export function usePageTitle() {
-  const location = useLocation();
+    const location = useLocation();
 
-  const title = useMemo(() => {
-    const current = router.find((route) => route.to === location.pathname);
-    return current?.name ?? "Not Found Page";
-  }, [location.pathname]);
+    const title = useMemo(() => {
+        const current = menuItems.find((route) => route.path === location.pathname);
+        return current?.label ?? "Not Found Page";
+    }, [location.pathname]);
 
-  return title;
+    return title;
 }
