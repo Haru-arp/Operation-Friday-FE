fd615ee678bde708e574ef03d4a36ad8a795f17c
Haru-arp <vr1997@naver.com>
2025-06-16 22:01:09 +0900

    refactor: transaction/new page

---
+++ src/api/index.ts
@@ -0,0 +1,7 @@
+// import { Configuration, DefaultApi } from "./generated"; // generated는 생성 경로
+// import axiosInstance from "@/lib/axios";
+
+// const config = new Configuration();
+// const api = new DefaultApi(config, undefined, axiosInstance);
+
+// export default api;
