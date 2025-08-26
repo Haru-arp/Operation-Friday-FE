944d14af69110d43865d5e85fcc1dd0b4ee56b54
dhlee <dhlee.xionits.com>
2025-08-26 10:53:57 +0900

    fix: api request rul fix

--- src/api/transactions.ts
+++ src/api/transactions.ts
@@ -1,4 +1,7 @@
-import type { ModifyTransactionRequest, TransactionRequest } from "@/components/ui/wizard/transaction-wizard";
+import type {
+  ModifyTransactionRequest,
+  TransactionRequest,
+} from "@/components/ui/wizard/transaction-wizard";
 import axiosInstance from "./axios";
 
 // export const login = async (form: LoginForm) => {
@@ -6,31 +9,39 @@ import axiosInstance from "./axios";
 //     return response.data;
 // };
 export const loadTransactions = async () => {
-    const response = await axiosInstance.get("/api/v1/transactions");
-    return response;
+  const response = await axiosInstance.get("/api/v1/transactions");
+  return response;
 };
 
 export const loadTransactionsById = async (id: number) => {
-    const response = await axiosInstance.get(`/api/v1/transactions/${id}`);
-    return response;
+  const response = await axiosInstance.get(`/api/v1/transactions/${id}`);
+  return response;
 };
 
 export const loadAccount = async () => {
-    const response = await axiosInstance.get(`/api/v1/accounts`);
-    return response;
+  const response = await axiosInstance.get(`/api/v1/accounts`);
+  return response;
 };
 
 export const registTransactions = async (form: TransactionRequest) => {
-    const response = await axiosInstance.post(`api/v1/transactions`, form);
-    return response;
+  const response = await axiosInstance.post(`/api/v1/transactions`, form);
+  return response;
 };
 
-export const modifyTransaction = async (transactionId: number, form: ModifyTransactionRequest) => {
-    const response = await axiosInstance.put(`api/v1/transactions/${transactionId}`, form);
-    return response;
+export const modifyTransaction = async (
+  transactionId: number,
+  form: ModifyTransactionRequest
+) => {
+  const response = await axiosInstance.put(
+    `/api/v1/transactions/${transactionId}`,
+    form
+  );
+  return response;
 };
 
 export const deleteTransaction = async (transactionId: number) => {
-    const response = await axiosInstance.delete(`api/v1/transactions/${transactionId}`);
-    return response;
+  const response = await axiosInstance.delete(
+    `/api/v1/transactions/${transactionId}`
+  );
+  return response;
 };
