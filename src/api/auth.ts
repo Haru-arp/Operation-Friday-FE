151c407e662ccc3236ff7f8b652e7c5b4695b368
dhlee <dhlee.xionits.com>
2025-08-26 10:55:34 +0900

    fix: fix request url

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,46 +1,46 @@
 import axiosInstance from "./axios";
 
 export interface LoginForm {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 export interface SignupForm {
-    email: string;
-    password: string;
-    name: string;
+  email: string;
+  password: string;
+  name: string;
 }
 
 export const login = async (form: LoginForm) => {
-    const response = await axiosInstance.post("/api/v1/users/login", form, {
-        withCredentials: true,
-    });
-    return response.data;
+  const response = await axiosInstance.post("/api/v1/users/login", form, {
+    withCredentials: true,
+  });
+  return response.data;
 };
 
 export const signup = async (form: SignupForm) => {
-    const response = await axiosInstance.post("/api/v1/users/signup", form);
-    return response.data;
+  const response = await axiosInstance.post("/api/v1/users/signup", form);
+  return response.data;
 };
 
 export const refresh = async () => {
-    const response = await axiosInstance.post(
-        "/api/v1/users/refresh",
-        {},
-        {
-            withCredentials: true,
-        }
-    );
-    return response.data.data.accessToken;
+  const response = await axiosInstance.post(
+    "/api/v1/users/refresh",
+    {},
+    {
+      withCredentials: true,
+    }
+  );
+  return response.data.data.accessToken;
 };
 export const logout = async () => {
-    const response = await axiosInstance.delete("/api/v1/users/logout", {
-        withCredentials: true,
-    });
-    return response.data;
+  const response = await axiosInstance.delete("/api/v1/users/logout", {
+    withCredentials: true,
+  });
+  return response.data;
 };
 
 export const getUserInfo = async () => {
-    const response = await axiosInstance.get("api/v1/users/me");
-    return response.data;
+  const response = await axiosInstance.get("/api/v1/users/me");
+  return response.data;
 };
